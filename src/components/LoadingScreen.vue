a539c0c7eff24c3d91ae562408ed611601e721d0
mortiz95 <53878835+mortiz95@users.noreply.github.com>
2022-03-26 10:35:45 +0000

    first commit Loading Screen

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="page-loader" v-if="!isloaded">
+        <ion-spinner></ion-spinner>
+ </div>
+  
+
+</template>
+
+<script>
+import { IonSpinner } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    data: () => {
+      return {
+        isloaded: false
+      }
+    },
+      mounted() {
+      document.onreadystatechange = () => {
+        if (document.readyState == "complete") { 
+          this.isloaded = true;
+        } 
+      }
+    },
+    components: { IonSpinner },
+    
+});
+</script>
+
+<style lang="scss" scoped>
+  body  {
+  background: url("./assets/landing.png") no-repeat center center
+  background-color: #cccccc;
+}
+
+  
+</style>
